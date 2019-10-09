55bea1aa0b7bdd680aefbec1d41a8ba461c4dea3
ogluisg <Luisanchez9821@Gmail.com>
2019-10-09 17:29:43 -0500

    Hello world program
    Hello world with a twist... in spanish!

--- java/Helloworld.java
+++ java/Helloworld.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package helloworld;
-
 /**
  *
  * @author student
  */
-public class Helloworld {
-
-    /**
-     * @param args the command line arguments
-     */
+public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    
 }
